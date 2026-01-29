a93d8574ddb6def15934f923be984cb88eeb70e7
znotfireman <znotfireman@gmail.com>
2026-01-29 16:38:24 +0700

    fix: an iq too high? (real)

--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,7 @@ const config = {
 			strict: true,
 		}),
 		prerender: {
-			entries: [],
+			entries: ["*"],
 		},
 		paths: {
 			base: process.argv.includes("dev") ? "" : process.env.BASE_PATH,
